7f147b1fb467416e2627329ec8b3d6a64a0eca92
chenlite <47260863+chenlite@users.noreply.github.com>
2019-02-03 12:53:31 -0200

    Update TESTE GFT.cob

--- TESTE GFT.cob
+++ TESTE GFT.cob
@@ -38,7 +38,7 @@ WORKING-STORAGE SECTION. 
           10 TAB-DISH    OCCURS 4 TIMES. 
              15 TAB-PRATO  PIC X(06). 
  
-1 SAIDA. 
+01 SAIDA. 
      05 SAI-DISH OCCURS 8 TIMES. 
           10 SAI-PRATO     PIC X(10). 
           10 SAI-VIRGULAS  PIC X(01). 
